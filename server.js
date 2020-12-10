39b5ffd4c50abd9e452f83de4f61e2612003fe6f
satishbodke <satbodake@gmail.com>
2020-12-10 18:26:58 +0000

    update team name

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ var request = require('request');
 const app = express();
 
 // setup team's name
-const teamName = 'gcp team';
+const teamName = 'Team X-Men';
 
 // set up handlbars as the templating engine
 app.set('view engine', 'hbs');
